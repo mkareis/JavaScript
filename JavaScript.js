33c0075989b6137c8cfe69b4d59911eb83a7c469
Mike <mike.s.o.reis2012@gmail.com>
2025-08-04 15:38:40 -0300

    Primeiro commit

---
+++ JavaScript.js
@@ -0,0 +1,10 @@
+var texto = 'Olá mundo';
+var inteiro = 500;
+var real = 50.5;
+var booleano = true;
+var nome = 'José';
+
+document.write("Olá " + nome + "<br>");
+document.write("O valor inteiro é de: " + inteiro + "<br>");
+document.write("O valor real é de: " + real + "<br>");
+document.write("A afirmação é: " + booleano + "<br>");
